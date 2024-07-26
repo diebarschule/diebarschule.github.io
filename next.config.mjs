a3a1c03bd89a764058e2b8205aa3200db2918194
wschrisd <christopher.doering@workingsoftware.io>
2024-07-26 12:33:43 +0200

    feat: stuff

--- next.config.mjs
+++ next.config.mjs
@@ -14,8 +14,8 @@ import { unifiedConditional } from 'unified-conditional'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-  basePath: '/diebarschule.github.io',
-  assetPrefix: '/diebarschule.github.io/',
+  basePath: '/https://www.diebarschule.github.io',
+  assetPrefix: '/https://www.diebarschule.github.io/',
   output: 'export',
 }
 
