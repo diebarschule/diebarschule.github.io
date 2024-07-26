3c459a3c8a524b706a74883dfd240d4ae86b2d24
diebarschule <diebarschule@gmail.com>
2024-07-26 14:13:49 +0200

    Update constants.ts

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,35 +1,5 @@
 export const events = [
     {
-      title: 'Lektion 2',
-      date: '24-06-2024',
-      type: 'Workshop',
-      description: 'Was ist guter Service? - Werdet Profis in unseren Serviceabläufen.',
-    },
-    {
-      title: 'Lektion 3',
-      date: '01-07-2024',
-      type: 'Workshop',
-      description: 'How to stock it? - Bereitet euch den Barbereich optimal vor.',
-    },
-    {
-      title: 'Lektion 4',
-      date: '08-07-2024',
-      type: 'Workshop',
-      description: 'How to tend it? - Lernt die Techniken und das Verhalten an der Bar kennen.',
-    },
-    {
-      title: 'Lektion 5',
-      date: '15-07-2024',
-      type: 'Workshop',
-      description: 'How to create? - Entwerft eure eigenen Drinks.',
-    },
-    {
-      title: 'Lektion 6',
-      date: '22-07-2024',
-      type: 'Workshop',
-      description: 'Wodka, Gin und Korn - Klare Sache..',
-    },
-    {
       title: 'Lektion 7',
       date: '29-07-2024',
       type: 'Workshop',
@@ -37,38 +7,68 @@ export const events = [
     },
     {
       title: 'Lektion 8',
-      date: '05-08-2024',
+      date: '19-08-2024',
       type: 'Workshop',
       description: 'Whisk(e)y - uisge beatha.',
     },
     {
       title: 'Lektion 9',
-      date: '12-08-2024',
+      date: '26-08-2024',
       type: 'Workshop',
-      description: ' Wein, Schaum- und Perlwein - in vino veritas.',
+      description: 'Wein, Schaum- und Perlwein - in vino veritas.',
     },
     {
       title: 'Lektion 10',
-      date: '19-08-2024',
+      date: '02-09-2024',
       type: 'Workshop',
-      description: ' Bier und Craft Beer - Hopfen und Malz, Gott erhalt`s.',
+      description: 'Bier und Craft Beer - Hopfen und Malz, Gott erhalt`s.',
     },
     {
       title: 'Lektion 11',
-      date: '26-08-2024',
+      date: '09-09-2024',
       type: 'Workshop',
       description: 'Liköre und sonstige Spirituosen - eine Übersicht.',
     },
     {
       title: 'Lektion 12',
-      date: '02-09-2024',
+      date: '16-09-2024',
       type: 'Workshop',
       description: 'How to manage? - Lernt ein Team zu führen und eine Bar eigenständig zu managen.',
     },
-     {
+    {
       title: 'Lektion 1',
-      date: '17-06-2024',
+      date: '23-09-2024',
       type: 'Workshop',
       description: 'Wir über uns - lernt unser Unternehmen kennen.',
+    },
+    {
+      title: 'Lektion 2',
+      date: '30-09-2024',
+      type: 'Workshop',
+      description: ' Was ist guter Service? - Werdet Profis in unseren Serviceabläufen.',
+    },
+    {
+      title: 'Lektion 3',
+      date: '07-10-2024',
+      type: 'Workshop',
+      description: ' How to stock it? - Bereitet euch den Barbereich optimal vor.',
+    },
+    {
+      title: 'Lektion 4',
+      date: '14-10-2024',
+      type: 'Workshop',
+      description: 'How to tend it? - Lernt die Techniken und das Verhalten an der Bar kennen.',
+    },
+    {
+      title: 'Lektion 5',
+      date: '21-10-2024',
+      type: 'Workshop',
+      description: 'How to create? - Entwerft eure eigenen Drinks.',
+    },
+     {
+      title: 'Lektion 6',
+      date: '28-10-2024',
+      type: 'Workshop',
+      description: 'Wodka, Gin und Korn - Klare Sache.',
     },
   ]
