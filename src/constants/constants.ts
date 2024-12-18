caf6efdd36140631b2512b5d5e26c1aff2214cf6
diebarschule <diebarschule@gmail.com>
2024-12-18 15:19:41 +0100

    Update constants.ts

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,75 +1,75 @@
 export const events = [
+
    
-   
-   {
-      title: 'Lektion 7',
-      date: '25-11-2024',
-      type: 'Workshop',
-      description: 'R(h)um, Chachaca, Mezcal und Tequila - Spirituosen aus der Karibik und Lateinamerika.',
-    },
-   {
-      title: 'Lektion 8',
-      date: '02-12-2024',
-      type: 'Workshop',
-      description: 'Whisk(e)y - uisge beatha.',
-    },
-   {
-      title: 'Lektion 9',
-      date: '10-12-2024',
-      type: 'Ausflug',
-      description: 'Bier und Craft Beer - Exkursion zu BRLO',
-    },
     {
       title: 'Lektion 10',
-      date: '16-12-2024',
+      date: '13-01-2025',
       type: 'Workshop',
       description: 'Wein, Schaum- und Perlwein - in vino veritas.',
     },
     {
       title: 'Lektion 11',
-      date: '06-01-2025',
+      date: '20-01-2025',
       type: 'Workshop',
       description: 'Liköre und sonstige Spirituosen - eine Übersicht.',
     },
 {
       title: 'Lektion 12',
-      date: '13-01-2025',
+      date: '27-01-2025',
       type: 'Workshop',
       description: 'How to manage? - Lernt ein Team zu führen und eine Bar eigenständig zu managen.',
     },
 { title: 'Lektion 1',
-      date: '20-01-2025',
+      date: '03-02-2025',
       type: 'Workshop',
       description: 'Wir über uns - lernt unser Unternehmen kennen.',
     },
 {
       title: 'Lektion 2',
-      date: '27-01-2025',
+      date: '10-02-2025',
       type: 'Workshop',
       description: ' Was ist guter Service? - Werdet Profis in unseren Serviceabläufen.',
     },
 {
       title: 'Lektion 3',
-      date: '03-02-2025',
+      date: '17-02-2025',
       type: 'Workshop',
       description: ' How to stock it? - Bereitet euch den Barbereich optimal vor.',
     }, 
 {
       title: 'Lektion 4',
-      date: '10-02-2025',
+      date: '24-02-2025',
       type: 'Workshop',
       description: 'How to tend it? - Lernt die Techniken und das Verhalten an der Bar kennen.',
     },
 {
       title: 'Lektion 5 - Special',
-      date: '17-02-2025',
+      date: '03-03-2025',
       type: 'Exkursion & Workshop',
       description: 'How to create? - Entwerft eure eigenen Drinks, diesmal zu Besuch im Lab!'
     },
 {
       title: 'Lektion 6',
-      date: '24-02-2025',
+      date: '10-03-2025',
       type: 'Workshop',
       description: 'Wodka, Gin und Korn - Klare Sache.',
+    },
+   {
+      title: 'Lektion 7',
+      date: '17-03-2025',
+      type: 'Workshop',
+      description: 'R(h)um, Chachaca, Mezcal und Tequila - Spirituosen aus der Karibik und Lateinamerika.',
+    },
+   {
+      title: 'Lektion 8',
+      date: '24-03-2025',
+      type: 'Workshop',
+      description: 'Whisk(e)y - uisge beatha.',
+    },
+   {
+      title: 'Lektion 9',
+      date: '31-03-2025',
+      type: 'Workshop',
+      description: 'Bier und Craft Beer',
     }]
      
